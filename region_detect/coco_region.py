c24572d5710e69dc9882063b2aeafaf85d953f78
lizhihao6 <lizhihao6@outlook.com>
2018-12-06 23:40:31 +0800

    add  function rm_write

--- region_detect/coco_region.py
+++ region_detect/coco_region.py
@@ -1,39 +1,53 @@
 import pylab
 import skimage.io as io
 import cv2
+import numpy as np
 from pycocotools.coco import COCO
 
-from .utils import coco2pic
+from .utils import COCO_Utils
 
 def get_anns(img_type, img_num):
     """
-    Used to trans coco data into regions. Save it to png.
+    Used to trans coco data into regions. Save it to several regions png.
     input:
         - img_type: "train", "val", "test"
     return:
         - None
     """
-    pylab.rcParams["figure.figsize"] = (8.0, 10.0)
-    dataDir = "coco"
-    dataType = "{}2017".format(img_type)
-    annFile = "{}/annotations/instances_{}.json".format(dataDir, dataType)
-    coco=COCO(annFile)
+    dataDir = 'coco'
+    dataType = '{}2017'.format(img_type)
+    annFile = '{}/annotations/instances_{}.json'.format(dataDir, dataType)
+    coco = COCO(annFile)
     counter = 0
+    number_list = []
     while counter < img_num:
-        number = np.random.randint(581782)
-        imgIds = coco.getImgIds(imgIds = [number])
+        number = np.random.randint(110, 581929)
+        if number in number_list:
+            continue
+        imgIds = coco.getImgIds(imgIds=[number])
         try:
-    	    img = coco.loadImgs(imgIds[np.random.randint(0, len(imgIds))])[0]
+            img = coco.loadImgs(imgIds[np.random.randint(0, len(imgIds))])[0]
         except KeyError:
             continue
-        I = io.imread(img["coco_url"])
-        annIds = coco.getAnnIds(imgIds=img["id"], iscrowd=0)
+        try:
+            I = io.imread(img['coco_url'])
+        except:
+            continue
+        annIds = coco.getAnnIds(imgIds=img['id'], iscrowd=0)
         anns = coco.loadAnns(annIds)
-        path = "{}_origin/{}.jpg".format(img_type, counter)
+        if len(anns) > 32:
+            continue
+        try:
+            anns[0]['segmentation']
+        except IndexError:
+            continue
+        path = '{}_origin/{}.png'.format(img_type, counter)
         cv2.imwrite(path, I)
-        path = "{}_segmetation/{}.jpg".format(img_type, counter)
-        coco2pic(I, anns, path)
+        path = "{}_coco2pic/{}".format(img_type, counter)
+        COCO_Utils.coco2pic(I, anns, path)
         counter += 1
+        print("finished coco region detect {} {} imgIds:{}".format(img_type, counter, imgIds))
+        number_list.append(number)
 
 if __name__ == '__main__':
     get_anns("train", 350)
--- region_detect/utils.py
+++ region_detect/utils.py
@@ -45,31 +45,46 @@ class Universe():
                 y.rank += 1
 
 
-def coco2pic(img, ann, path):
-    plt.figure(0)
-    plt.imshow(np.zeros_like(img, dtype=np.int8))
-    plt.axis("off")
-    c = 6.
-    polygons = []
-    colors = []
-    segs = [ann[i]['segmentation'][0] for i in range(len(ann))]
-    for seg in segs:
-        poly = np.array(seg).reshape(int(len(seg)/2), 2)
-        polygons.append(Polygon(poly))
-        color = np.zeros((1, 3))
-        color[0,0] += c/255. 
-        color[0, 1] += (1.-c/255.)
-        colors.append(color.tolist()[0])
-        c += 6.
-    ax = plt.gca()
-    ax.set_autoscale_on(False)
-    p = PatchCollection(polygons, facecolor=colors, linewidths=0, alpha=1)
-    ax.add_collection(p)
-    plt.savefig(path)
-    plt.close(0)
-    im = cv2.imread(path)
-    a = im.shape + img.shape
-    b = im.shape - img.shape
-    im_ = np.zeros_like(img, dtype=np.int8)
-    im_ = im[b[0]/2:a[0]/2, b[1]/2, a[1]/2, :]
-    cv2.imwrite(path, im_)
+class COCO_Utils:
+
+    @staticmethod
+    def coco2pic(img, ann, path):
+        img_shape = img.shape
+        segs = [ann[i]['segmentation'][0] for i in range(len(ann))]
+        counter = 0
+        for seg in segs:
+            poly = np.array(seg).reshape(int(len(seg)/2), 2)
+            polygon = Polygon(poly)
+            _path = "{}_{}.png".format(path, counter)
+            COCO_Utils.polygon2pic(img_shape, polygon, _path)
+
+    @staticmethod
+    def polygon2pic(img_shape, polygon, path):
+        plt.figure(0)
+        plt.imshow(np.zeros(img_shape))
+        plt.axis("off")
+        ax = plt.gca()
+        ax.set_autoscale_on(False)
+        color = np.ones((1, 3)) * 0.5
+        color = color.tolist()[0]
+        p = PatchCollection([polygon], facecolor=[color],
+                            linewidths=0, alpha=1)
+        ax.add_collection(p)
+        plt.savefig(path)
+        plt.close(0)
+        COCO_Utils.rm_white(img_shape, path)
+
+    @staticmethod
+    def rm_white(img_shape, path):
+        im = cv2.imread(path)
+        _im = np.zeros(img_shape)
+        white = True
+        j = 0
+        while(white):
+            for i in range(im.shape[1]):
+                white = (im[j, i, 0] == 255) and (im[j, i, 1] == 255) and (im[j, i, 2] == 255)
+                if not white:
+                    _im[:, :, :] = im[j: img_shape[0] + j, i: img_shape[1] + i, :]
+                    break
+            j += 1
+        cv2.imwrite(path, _im)
