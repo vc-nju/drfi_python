045fb3bd7e17876034125346a7b715b557520f4b
lizhihao6 <lizhihao6@outlook.com>
2018-12-11 19:58:32 +0800

    add igonre

--- feature_process/utils.py
+++ feature_process/utils.py
@@ -113,48 +113,53 @@ class Utils():
                  the storage sequence is corrdinated to edge_neigh: for Ri, the nei_point[i][j] means the edge point between Ri and Rj
                  may be a bit confusing, good luck!
         '''
-        shape = (self.rmat[0], self.rmat[1], 8, )
+        shape = (rmat.shape[0], rmat.shape[1], 8, )
         edge_mat = np.zeros(shape, dtype=np.int32)
-        edge_mat[:-1, :, 0] += (self.rmat[1:, :] - self.rmat[:-1, :])
-        edge_mat[1:, :, 1] += (self.rmat[:-1, :] - self.rmat[1:, :])
-        edge_mat[:, :-1, 2] += (self.rmat[:, 1:] - self.rmat[:, :-1])
-        edge_mat[:, 1:, 3] += (self.rmat[:, :-1] - self.rmat[:, 1:])
-        edge_mat[:-1, :-1, 4] += (self.rmat[1:, 1:] - self.rmat[:-1, :-1])
-        edge_mat[:-1, 1:, 5] += (self.rmat[1:, :-1] - self.rmat[:-1, 1:])
-        edge_mat[1:, :-1, 6] += (self.rmat[:-1, 1:] - self.rmat[1:, :-1])
-        edge_mat[1:, 1:, 7] += (self.rmat[:-1, :-1] - self.rmat[1:, 1:])
+        edge_mat[:-1, :, 0] += (rmat[1:, :] - rmat[:-1, :])
+        edge_mat[1:, :, 1] += (rmat[:-1, :] - rmat[1:, :])
+        edge_mat[:, :-1, 2] += (rmat[:, 1:] - rmat[:, :-1])
+        edge_mat[:, 1:, 3] += (rmat[:, :-1] - rmat[:, 1:])
+        edge_mat[:-1, :-1, 4] += (rmat[1:, 1:] - rmat[:-1, :-1])
+        edge_mat[:-1, 1:, 5] += (rmat[1:, :-1] - rmat[:-1, 1:])
+        edge_mat[1:, :-1, 6] += (rmat[:-1, 1:] - rmat[1:, :-1])
+        edge_mat[1:, 1:, 7] += (rmat[:-1, :-1] - rmat[1:, 1:])
         edge_mat[edge_mat!=0] = 1
         generate_y_list = lambda y: [y+1, y-1, y, y, y+1, y+1, y-1, y-1]
         generate_x_list = lambda x: [x, x, x+1, x-1, x+1, x-1, x+1, x-1]
-        shape = (self.rmat[0], self.rmat[1], 2, 8)
-        y_x = np.zeros(shape)
+        shape = (rmat.shape[0], rmat.shape[1], 2, 8)
+        y_x = np.zeros(shape, dtype=np.int32)
         for y in range(shape[0]):
             for x in range(shape[1]):
                 y_x[y,x,0,:] = generate_y_list(y)
                 y_x[y,x,1,:] = generate_x_list(x)
-        edge_nums = np.zeros([len(self.rlist), 1])
+        edge_nums = []
         edge_neigh = []
         edge_point = []
         append_not_exist = lambda x, _list: _list.append(x) if x not in _list else _list
-        for region in self.rlist:
+        for region in rlist:
             num = 0
             neighs = []
             points = []
             for y, x in zip(region[0], region[1]):
-                for edge_direct in edge_mat.shape[2]:
-                    if edge_direct[y, x, edge_direct] != 0:
+                for edge_direct in range(edge_mat.shape[2]):
+                    if edge_mat[y, x, edge_direct] != 0:
                         y_ = y_x[y, x, 0, edge_direct]
                         x_ = y_x[y, x, 1, edge_direct]
                         num += 1
-                        points = append_not_exist((y_, x_, ), points)
-                        neighs = append_not_exist(self.rmat[y_, x_], neighs)
+                        neigh_id = rmat[y_, x_]
+                        if neigh_id not in neighs:
+                            neighs.append(neigh_id)
+                        p = (y_, x_,)
+                        if p not in points:
+                            points.append(p)
             edge_nums.append(num)
+            assert(len(neighs) != 0)
             edge_neigh.append(neighs)
             _points = [(),()]
             for i in range(len(points)):
                 _points[0] += (points[i][0],)
                 _points[1] += (points[i][1],)
-            edge_point.append(tuple(_points))  
+            edge_point.append(tuple(_points))
         # edge_nums /= self.width*self.height
         return edge_nums, edge_neigh, edge_point
 
