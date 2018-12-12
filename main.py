6b91029787bea0802be9b4a57f79643ab05c8889
lizhihao6 <lizhihao6@outlook.com>
2018-12-12 16:40:25 +0800

    small change

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ from region_detect import Super_Region, Region2Csv
 
 def save_csv(img_type, img_id):
     path = "data/{}_origin/{}.png".format(img_type, img_id)
-    rlist, rmat = Super_Region.get_region(path, 100.)
+    rlist, rmat = Super_Region.get_region(path, 300.)
     features = Features(path, rlist, rmat)
     Region2Csv(features, rlist,img_type,img_id)
 
 if __name__ == "__main__":
     for i in range(450):
         print("train", i)
-        save_csv("train", i)
+        try:
+            save_csv("train", i)
+        except:
+            print("failed in train", i)
     for i in range(50):
         print("val", i)
-        save_csv("val", i)
+        try:
+            save_csv("val", i)
+        except:
+            print("failed in val", i)
